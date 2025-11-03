cf2abaea7046b4556a0fd9b6551b053274e958ec
kevinalexislopezcamacho <kevinalexislopezcamacho@gmail.com>
2025-11-03 16:43:56 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { PokemonProvider } from './context/PokemonContext'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PokemonProvider>
+      <App />
+    </PokemonProvider>
   </React.StrictMode>
 )
